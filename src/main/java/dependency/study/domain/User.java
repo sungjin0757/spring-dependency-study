f4bfa39b24c61cda689e0bd024203986d8376af2
sungjin <sungjin0757@naver.com>
2021-08-31 03:02:12 +0900

    초기 설정

---
+++ src/main/java/dependency/study/domain/User.java
@@ -0,0 +1,15 @@
+package dependency.study.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private Long id;
+    private String name;
+    private String password;
+
+}
---
+++ src/main/java/dependency/study/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package dependency.study.repository;
+
+import dependency.study.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@RequiredArgsConstructor
+@Repository
+public class UserRepository {
+
+    private final EntityManager em;
+
+    public Long save(User user){
+        em.persist(user);
+        return user.getId();
+    }
+
+    public User findOne(Long id){
+        return em.find(User.class,id);
+    }
+
+    public void remove(Long id){
+        User user=findOne(id);
+        em.remove(user);
+    }
+}
